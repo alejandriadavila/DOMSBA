64bee3964460738f0b38a6239f1ef6ffb9889400
alejandriadavila <149810746+alejandriadavila@users.noreply.github.com>
2023-12-15 18:54:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Making the header
+const headingEl = document.querySelector("header")
+const h1 = document.createElement("h1")
+h1.innerText = "Baby Park Tournament Sign Ups"
+headingEl.appendChild(h1)
+headingEl.classList.add("flex-center")
+
+const email = document.createElement("input")
